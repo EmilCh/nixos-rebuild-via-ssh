d6bbd0d60f69c8c8b6bf289a0a8bd710a3027312
Emil C <emil@cheriches.com>
2025-08-20 17:56:01 +0200

    mer. 20 août 2025 17:56:01 CEST

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       ...
     }:
     {
-      # nixos-rebuild --flake .#generic switch --target-host <hostname>
+      # nixos-rebuild --flake .#generic switch --no-write-lock-file --upgrade --target-host <hostname>
       nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
